b285621ed62b98714ac7f1ff8c7aebf3afef484f
Prince Elorm <princeelorm17@gmail.com>
2024-09-23 01:57:18 +0000

    added toast notifications

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,13 @@
 import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import FormField from "@/components/form-field";
 import CustomButton from "@/components/custombutton";
 import { router, Link } from "expo-router";
 import { createUser } from "@/lib/appwrite";
-
+import { toast, Toasts } from "@backpackapp-io/react-native-toast";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 const SignUp = () => {
   const [form, setForm] = React.useState({
     username: "",
@@ -29,12 +30,14 @@ const SignUp = () => {
 
   const submitInfo = async () => {
     if (!form.username || !form.email || !form.password) {
-      return Alert.alert("All fields are required");
+      return toast.error("All fields are required", {
+        duration: 3000,
+      });
     }
 
     const validEmail = emailRegex(form.email);
     if (!validEmail) {
-      return Alert.alert("Please enter a valid email address");
+      return toast.error("Please enter a valid email address");
     }
 
     setSubmitting(true);
@@ -44,70 +47,82 @@ const SignUp = () => {
 
       if (res) {
         console.log(res);
+        toast.success(
+          `User created successfully with username ${form.username}}`
+        );
         router.push("/home");
       }
     } catch (error: any) {
-      Alert.alert("Error", error?.message || "Something went wrong");
+      toast.error(error?.message || "Something went wrong");
     } finally {
       setSubmitting(false);
     }
   };
 
   return (
-    <SafeAreaView className="h-full bg-primary">
-      <ScrollView>
-        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
-          <View className="flex flex-row items-center gap-4 mt-2">
-            <Ionicons name="book-outline" size={45} color="white" />
-            <Text className="text-5xl tracking-tighter text-white font-pextrabold">
-              bookr
-            </Text>
-          </View>
+    <SafeAreaProvider>
+      <GestureHandlerRootView className="h-full">
+        <SafeAreaView className="h-full bg-primary">
+          <ScrollView>
+            <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+              <View className="flex flex-row items-center gap-4 mt-2">
+                <Ionicons name="book-outline" size={45} color="white" />
+                <Text className="text-5xl tracking-tighter text-white font-pextrabold">
+                  bookr
+                </Text>
+              </View>
 
-          <Text className="mt-10 text-2xl tracking-tighter text-white font-psemibold">
-            Sign up into <Text className="text-secondary-200">bookr</Text>
-          </Text>
+              <Text className="mt-10 text-2xl tracking-tighter text-white font-psemibold">
+                Sign up into <Text className="text-secondary-200">bookr</Text>
+              </Text>
 
-          <FormField
-            title="Username"
-            value={form.username}
-            handleChangeText={(e: string) => setForm({ ...form, username: e })}
-            otherStyles="mt-7"
-          />
-          <FormField
-            title="Email"
-            value={form.email}
-            handleChangeText={(e: string) => setForm({ ...form, email: e })}
-            otherStyles="mt-7 mb-7"
-          />
-          <FormField
-            title="Password"
-            value={form.password}
-            handleChangeText={(e: string) => setForm({ ...form, password: e })}
-            otherStyles="mb-7"
-          />
+              <FormField
+                title="Username"
+                value={form.username}
+                handleChangeText={(e: string) =>
+                  setForm({ ...form, username: e })
+                }
+                otherStyles="mt-7"
+              />
+              <FormField
+                title="Email"
+                value={form.email}
+                handleChangeText={(e: string) => setForm({ ...form, email: e })}
+                otherStyles="mt-7 mb-7"
+              />
+              <FormField
+                title="Password"
+                value={form.password}
+                handleChangeText={(e: string) =>
+                  setForm({ ...form, password: e })
+                }
+                otherStyles="mb-7"
+              />
 
-          <CustomButton
-            title="Sign Up"
-            handlePress={submitInfo}
-            containerStyles="mt-7"
-            isLoading={isSubmitting}
-          />
+              <CustomButton
+                title="Sign Up"
+                handlePress={submitInfo}
+                containerStyles="mt-7"
+                isLoading={isSubmitting}
+              />
 
-          <View className="flex-row justify-center gap-2 pt-5">
-            <Text className="text-lg text-gray-100 font-pregular">
-              Already got an account?
-            </Text>
-            <Link
-              href={"/sign-in"}
-              className="text-lg text-secondary-200 font-psemibold"
-            >
-              Sign In
-            </Link>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+              <View className="flex-row justify-center gap-2 pt-5">
+                <Text className="text-lg text-gray-100 font-pregular">
+                  Already got an account?
+                </Text>
+                <Link
+                  href={"/sign-in"}
+                  className="text-lg text-secondary-200 font-psemibold"
+                >
+                  Sign In
+                </Link>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+        <Toasts />
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 };
 
